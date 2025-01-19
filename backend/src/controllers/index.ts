41735b0cbf4e99fd68ebd413c20c04c6b6e26312
Makan <contact@makantayebi.com>
2025-01-19 03:01:59 +0100

    semantics

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -1,4 +1,4 @@
 /** @format */
 
 export * as authController from "./auth.controller";
-export * as sentimentAnalyzerController from "./sentiment.analyzer.controller";
+export * as sentimentAnalyzerController from "./text.controller";
